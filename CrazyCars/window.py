1a792b78e8fc0d16b1e607413beeac418a19e78b
sharegoat98 <aleksa.berdih@gmail.com>
2021-01-22 21:58:48 +0100

    PlayScreen za multiplayer

--- CrazyCars/window.py
+++ CrazyCars/window.py
@@ -73,12 +73,22 @@ class MainWindow(QWidget):
         label_4.setObjectName("label_4")
 
         playButton = QtWidgets.QPushButton(self)
-        playButton.setGeometry(QtCore.QRect(160, 500, 211, 71))
+        playButton.setGeometry(QtCore.QRect(290, 500, 211, 71))
         font = QtGui.QFont()
         font.setFamily("Playbill")
         font.setPointSize(24)
         playButton.setFont(font)
         playButton.setStyleSheet("background-color:orange")
+
+        #dugme za multiplejer
+        playButtonMulti = QtWidgets.QPushButton(self)
+        playButtonMulti.setGeometry(QtCore.QRect(50, 500, 211, 71))
+        font = QtGui.QFont()
+        font.setFamily("Playbill")
+        font.setPointSize(24)
+        playButtonMulti.setFont(font)
+        playButtonMulti.setStyleSheet("background-color:orange")
+
 
         exitButton = QtWidgets.QPushButton(self)
         exitButton.setGeometry(QtCore.QRect(160, 600, 211, 71))
@@ -88,17 +98,23 @@ class MainWindow(QWidget):
         exitButton.setFont(font)
         exitButton.setStyleSheet("background-color:orange")
 
-        playButton.setText("START")
+        playButton.setText("Single player")
+        playButtonMulti.setText("Multiplayer")
         exitButton.setText("QUIT")
 
         exitButton.clicked.connect(QCoreApplication.instance().quit)
         playButton.clicked.connect(self.playScreen)
+        playButtonMulti.clicked.connect(self.playScreenMulti)
 
     def playScreen(self):
         self.w = PlayWindow()
         self.w.show()
         self.hide()
 
+    def playScreenMulti(self):
+        self.w = PlayWindowMulti()
+        self.w.show()
+        self.hide()
 
 class PlayWindow(QWidget):
 
@@ -205,6 +221,119 @@ class PlayWindow(QWidget):
         qp.drawLine(235, 0, 235, 700)
         qp.drawLine(315, 0, 315, 700)
         qp.drawLine(395, 0, 395, 700)
+
+
+class PlayWindowMulti(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setFixedSize(550, 700)
+        self.setWindowTitle('Crazy Cars')
+        self.setWindowIcon(QIcon('Slike/toy-red-car-isolated-on-white-background-donald-erickson.jpg'))
+
+        self.center()
+        self.mainScreen()
+        self.show()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def mainScreen(self):
+        exit_button = QPushButton("")
+        exit_button.setIcon(QIcon('Slike/pause3.png'))
+        exit_button.setStyleSheet("border: 0px solid black")
+        exit_button.setToolTip("Pause game")
+        exit_button.setFixedWidth(50)
+        exit_button.setFixedHeight(50)
+        oImage = QImage("Slike/put2.png")
+        palette = QPalette()
+        palette.setBrush(QPalette.Window, QBrush(oImage))
+        self.setPalette(palette)
+
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(exit_button)
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addLayout(hbox)
+        self.setLayout(vbox)
+
+
+        self.process=QProcess(self)
+        self.process.started.connect(lambda: self.paintEvent(self))
+        self.process.start()
+        #self.paintEvent(self)
+
+
+        #scores
+        self.score = Scores(self)
+
+        #avatars
+        self.a1 = Avatar(self,170)
+        #self.a1.x=150
+        self.a2 = Avatar(self,330)
+        #self.a2.x=350
+
+        self.tO= TimerObjects(self,self.a1, self.score)
+
+        self.thread = QThread()
+        self.moveToThread(self.thread)
+        self.thread.started.connect(self.tO.generateObjectWithTimer)
+        self.thread.start()
+
+
+        #Pokretanje sa tajmerom-Prvi nacin
+        #self.tO.generateObjectWithTimer()
+
+        #Pokretanje drugi nacin
+        #self.tO.pokreniThread()
+
+        #Pokretanje treci nacin
+        #self.tO.generateObjectWithoutTimer()
+
+        exit_button.clicked.connect(self.pauseScreen)
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_A:
+            self.a1.moveMeLeft()
+        if event.key() == QtCore.Qt.Key_D:
+            self.a1.moveMeRight()
+        if event.key() == QtCore.Qt.Key_J:
+            self.a2.moveMeLeft()
+        if event.key() == QtCore.Qt.Key_L:
+            self.a2.moveMeRight()
+
+
+
+    def pauseScreen(self):
+        self.wp = PauseWindow()
+        self.wp.show()
+        self.hide()
+
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.drawLines(qp)
+        qp.end()
+
+    def drawLines(self, qp):
+        pen = QPen(Qt.white, 2, Qt.SolidLine)
+        pen.setStyle(Qt.DashLine)
+        qp.setPen(pen)
+        qp.drawLine(155, 0, 155, 700)
+        qp.drawLine(235, 0, 235, 700)
+        qp.drawLine(315, 0, 315, 700)
+        qp.drawLine(395, 0, 395, 700)
+
+
+
 
 
 class PauseWindow(QWidget):
